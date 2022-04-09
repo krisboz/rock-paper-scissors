6e1955f4e73ca30d3e3b3ebf8867e85fd68997a1
krisboz <kristijan.bozinovic1@gmail.com>
2022-04-09 13:38:29 +0200

    ROck Paper Scissors made

--- index.js
+++ index.js
@@ -1,20 +1,70 @@
 const computerPlay = () => {
     const options = ["rock", "paper", "scissors"];
     let chosen = Math.floor(Math.random() * 3)
-    console.log(options[chosen])
+    return (options[chosen])
 }
+
+
+
+
+
+            
 
 
 
 const playRound = (playerSelection, computerSelection) => {
 
-    //rock beats scissors
-    //paper beats rock
-    //scissors beat paper
+    document.getElementById("player-image").src=`./images/${playerSelection}.png`
+    document.getElementById("computer-image").src=`./images/${computerSelection}.png`
 
     if (playerSelection === computerSelection) {
-        console.log("draw")
+        document.querySelector("#winner-val").innerHTML = "Draw"
+
+    } else if (playerSelection === "rock") {
+        if(computerSelection === "paper") {
+            document.querySelector("#winner-val").innerHTML = ("Computer wins")
+        } else {
+            document.querySelector("#winner-val").innerHTML = ("Player wins")
+        }
+    } else if (playerSelection === "paper") {
+        if (computerSelection === "scissors") {
+            document.querySelector("#winner-val").innerHTML = ("Computer wins")
+        } else {
+            document.querySelector("#winner-val").innerHTML = ("Player wins")
+        }
+    } else if (playerSelection === "scissors") {
+        if (computerSelection === "rock") {
+            document.querySelector("#winner-val").innerHTML = ("Computer wins")
+        } else {
+            document.querySelector("#winner-val").innerHTML = ("Player wins")
+        }
+    }
+
+    if (document.querySelector("#winner-val").innerHTML === "Computer wins") {
+        console.log("computer")
+    } else if (document.querySelector("#winner-val").innerHTML === "Player wins") {
+        console.log("player")
     }
 
 
 }
+
+
+
+
+const game = () => {
+
+
+    const handlePlayer = (event) => {
+        let button = event.target.id;
+        playRound(button, computerPlay());
+    }
+
+    document.querySelector(".player-selection").addEventListener("click", handlePlayer)
+
+
+    
+}
+
+
+game();
